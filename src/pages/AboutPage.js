89cbae1767284a3a986858ee95a035fdafed12cc
unknown <bipulpoudel55555@gmail.com>
2020-11-02 22:04:48 +0545

    New Update Completed

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import AboutUsDetail from '../components/AboutUsDetail'
-import CounterSection from '../components/CounterSection'
-import HowItWork from '../components/HowItWork'
-import OurTeamSection from '../components/OurTeamSection'
+import React from 'react';
+import AboutUsDetail from '../components/AboutUsDetail';
+import CounterSection from '../components/CounterSection';
+import HowItWork from '../components/HowItWork';
+import OurTeamSection from '../components/OurTeamSection';
 
 function AboutPage() {
     return (
         <>
-           <AboutUsDetail/>
-           <CounterSection/>
-           <HowItWork/> 
-           <OurTeamSection/>
+            <AboutUsDetail />
+            <CounterSection />
+            <HowItWork />
+            <OurTeamSection />
         </>
-    )
+    );
 }
 
-export default AboutPage
+export default AboutPage;
