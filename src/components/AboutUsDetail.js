6054cf87e488e20c665a3da1ba7a5d00c9b2b950
unknown <bipulpoudel55555@gmail.com>
2020-11-02 22:00:09 +0545

    New Updates

--- src/components/AboutUsDetail.js
+++ src/components/AboutUsDetail.js
@@ -1,28 +1,57 @@
-import React from 'react'
+import React from 'react';
 
 function AboutUsDetail() {
     return (
         <>
-        <section className="breadcrumb-section">
-        
-        </section>
+            <section className='breadcrumb-section'></section>
 
-        <section className="about-us-area section-padding">
-            <div className="container">
-                <div className="row">
-                <div className="col-lg-6">
-                    <h2 className="section-title">About us</h2>
-                    <div className="about-us-text">
-                    <h4>Trusted Cab Services in All World</h4>
-                    <p>Curabitur placerat cursus nisi nec pharetra. Proin quis tortor fringilla, placerat nisi nec, auctor ex. Donec commodo orci ac lectus mattis, sed interdum sem scelerisque. Vivamus at euismod magna. Aenean semper risus nec dolor bibendum cursus. Donec eu odio eu ligula sagittis fringilla. Phasellus vulputate velit eu vehicula auctor. Nam vel pellentesque libero. Duis eget efficitur dui. Mauris tempor ex non tortor aliquet, et interdum mi dapibus. Phasellus ac dui nunc. Sed quis sagittis lorem, in blandit nibh. Fusce dui metus, interdum ac malesuada eu, ornare nec neque. Fusce hendrerit, tortor id egestas rutrum, orci lorem lacinia velit, sed mollis augue diam eget ipsum. Curabitur euismod, tellus sit amet tincidunt semper, dui odio pharetra orci, sed molestie odio libero sed libero. Sed volutpat ornare mauris. Sed gravida pulvinar urna, eget euismod mi mattis a. Vivamus sagittis eu quam sed ullamcorper. Etiam aliquet ornare tempus. Maecenas dictum nunc ac tortor rutrum, quis sollicitudin libero feugiat. Mauris iaculis sed risus ut tempus.</p>
-                    <a href="/about" className="button button-dark tiny">Read More</a>
+            <section className='about-us-area section-padding'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-lg-6'>
+                            <h2 className='section-title'>About us</h2>
+                            <div className='about-us-text'>
+                                <h4>Trusted Cab Services in All World</h4>
+                                <p>
+                                    Curabitur placerat cursus nisi nec pharetra.
+                                    Proin quis tortor fringilla, placerat nisi
+                                    nec, auctor ex. Donec commodo orci ac lectus
+                                    mattis, sed interdum sem scelerisque.
+                                    Vivamus at euismod magna. Aenean semper
+                                    risus nec dolor bibendum cursus. Donec eu
+                                    odio eu ligula sagittis fringilla. Phasellus
+                                    vulputate velit eu vehicula auctor. Nam vel
+                                    pellentesque libero. Duis eget efficitur
+                                    dui. Mauris tempor ex non tortor aliquet, et
+                                    interdum mi dapibus. Phasellus ac dui nunc.
+                                    Sed quis sagittis lorem, in blandit nibh.
+                                    Fusce dui metus, interdum ac malesuada eu,
+                                    ornare nec neque. Fusce hendrerit, tortor id
+                                    egestas rutrum, orci lorem lacinia velit,
+                                    sed mollis augue diam eget ipsum. Curabitur
+                                    euismod, tellus sit amet tincidunt semper,
+                                    dui odio pharetra orci, sed molestie odio
+                                    libero sed libero. Sed volutpat ornare
+                                    mauris. Sed gravida pulvinar urna, eget
+                                    euismod mi mattis a. Vivamus sagittis eu
+                                    quam sed ullamcorper. Etiam aliquet ornare
+                                    tempus. Maecenas dictum nunc ac tortor
+                                    rutrum, quis sollicitudin libero feugiat.
+                                    Mauris iaculis sed risus ut tempus.
+                                </p>
+                                <a
+                                    href='/about'
+                                    className='button button-dark tiny'
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            </div>
-        </section>
-    </>
-    )
+            </section>
+        </>
+    );
 }
 
-export default AboutUsDetail
+export default AboutUsDetail;
