7775a0607a66f091067e246259375904639dcc81
unknown <bipulpoudel55555@gmail.com>
2020-11-02 22:04:36 +0545

    New Update Completed

--- src/layouts/wrapper.js
+++ src/layouts/wrapper.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MainLayout from './MainLayout';
 
-function RouteWrapper({
-    component: Component,
-    ...rest
-  }) {
+function RouteWrapper({ component: Component, ...rest }) {
     return (
-      <Route {...rest} render={(props) =>
-        <MainLayout {...props}>
-          <Component {...props} />
-        </MainLayout>
-      } />
+        <Route
+            {...rest}
+            render={props => (
+                <MainLayout {...props}>
+                    <Component {...props} />
+                </MainLayout>
+            )}
+        />
     );
 }
 
-export default RouteWrapper
+export default RouteWrapper;
