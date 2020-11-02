742fc28c1c2c0edffd5397553a9397ac4237e5dc
unknown <bipulpoudel55555@gmail.com>
2020-11-02 22:05:02 +0545

    New Update Completed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 //Import of Component
-import AboutUsSection from '../components/AboutUsSection'
-import HeroSection from '../components/HeroSection'
-import HowItWork from '../components/HowItWork'
+import AboutUsSection from '../components/AboutUsSection';
+import HeroSection from '../components/HeroSection';
+import HowItWork from '../components/HowItWork';
 import TestimonialSection from '../components/TestimonialSection';
 
 function HomePage() {
     return (
         <>
-            <HeroSection/>
-            <HowItWork/>
-            <AboutUsSection/>
-            <TestimonialSection/>
+            <HeroSection />
+            <HowItWork />
+            <AboutUsSection />
+            <TestimonialSection />
         </>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
