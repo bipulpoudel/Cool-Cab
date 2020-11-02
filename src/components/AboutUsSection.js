d1d21f1fe8a5dd91db61166f7fd945d061bbbd7b
unknown <bipulpoudel55555@gmail.com>
2020-11-02 22:00:35 +0545

    New updates

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 function AboutUsSection() {
     return (
         <>
-           <section class="about-us-area bg-2 section-padding">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <h2 class="section-title">About us</h2>
-                            <div class="about-us-text">
+            <section class='about-us-area bg-2 section-padding'>
+                <div class='container'>
+                    <div class='row'>
+                        <div class='col-lg-6'>
+                            <h2 class='section-title'>About us</h2>
+                            <div class='about-us-text'>
                                 <h4>Trusted Cab Services in All World</h4>
-                                <p>Curabitur placerat cursus nisi nec pharetra. Proin quis tortor fringilla, placerat nisi nec, auctor ex. Donec commodo orci ac lectus mattis, sed interdum sem scelerisque. Vivamus at euismod magna. Aenean semper risus nec dolor bibendum cursus. Donec eu odio eu ligula sagittis fringilla. Phasellus vulputate velit eu vehicula auctor. Nam vel pellentesque libero. Duis eget efficitur dui. Mauris tempor ex non tortor aliquet, et interdum mi dapibus. Phasellus ac dui nunc. Sed quis sagittis lorem, in blandit nibh.</p>
-                                <a href="/about" class="button button-dark tiny">Read More</a>
+                                <p>
+                                    Curabitur placerat cursus nisi nec pharetra.
+                                    Proin quis tortor fringilla, placerat nisi
+                                    nec, auctor ex. Donec commodo orci ac lectus
+                                    mattis, sed interdum sem scelerisque.
+                                    Vivamus at euismod magna. Aenean semper
+                                    risus nec dolor bibendum cursus. Donec eu
+                                    odio eu ligula sagittis fringilla. Phasellus
+                                    vulputate velit eu vehicula auctor. Nam vel
+                                    pellentesque libero. Duis eget efficitur
+                                    dui. Mauris tempor ex non tortor aliquet, et
+                                    interdum mi dapibus. Phasellus ac dui nunc.
+                                    Sed quis sagittis lorem, in blandit nibh.
+                                </p>
+                                <a
+                                    href='/about'
+                                    class='button button-dark tiny'
+                                >
+                                    Read More
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section> 
+            </section>
         </>
-    )
+    );
 }
 
-export default AboutUsSection
+export default AboutUsSection;
