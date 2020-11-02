233b1582dd0fe49a18682575bf3ecf70373b51c2
unknown <bipulpoudel55555@gmail.com>
2020-11-02 21:59:00 +0545

    New Header implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router , Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 //Layouts Imports
 import RouteWrapper from './layouts/wrapper';
-import AboutPage from './pages/AboutPage';
 
 //page imports
 import HomePage from './pages/HomePage';
 import PackagePage from './pages/PackagePage';
 import ServicePage from './pages/ServicePage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <RouteWrapper exact path="/" component={HomePage}/>
-        <RouteWrapper exact path="/about" component={AboutPage}/>
-        <RouteWrapper exact path="/services" component={ServicePage}/>
-        <RouteWrapper exact path="/packages" component={PackagePage}/>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <RouteWrapper exact path='/' component={HomePage} />
+                <RouteWrapper exact path='/about' component={AboutPage} />
+                <RouteWrapper exact path='/services' component={ServicePage} />
+                <RouteWrapper exact path='/packages' component={PackagePage} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
