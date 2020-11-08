843d33fa82197621680326ece111d32f8e074589
unknown <bipulpoudel55555@gmail.com>
2020-11-08 11:20:16 +0545

    Basic Validation Added

--- src/components/SignInPart.js
+++ src/components/SignInPart.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 const signInSchema = Yup.object().shape({
     email: Yup.string()
         .email('Please enter a valid email')
         .required('Email is a required field'),
+    password: Yup.string()
+        .required('Password is a required field')
+        .min(8, 'Password length must be greater than 8'),
 });
 
 function SignInPart() {
@@ -22,7 +25,7 @@ function SignInPart() {
             }}
         >
             {({ errors, touched }) => (
-                <div className='user-access-form'>
+                <Form className='user-access-form'>
                     <div className='form-group'>
                         <input
                             type='email'
@@ -30,7 +33,11 @@ function SignInPart() {
                             aria-describedby='emailHelp'
                             placeholder='Enter your email address'
                         />
+                        {errors.email && touched.email ? (
+                            <div>{errors.email}</div>
+                        ) : null}
                     </div>
+
                     <div className='form-group'>
                         <input
                             type='password'
@@ -38,14 +45,20 @@ function SignInPart() {
                             id='exampleInputPassword1'
                             placeholder='Password'
                         />
+                        {errors.password && touched.password ? (
+                            <small id='emailHelp' class='form-text text-danger'>
+                                {errors.password}
+                            </small>
+                        ) : null}
                     </div>
+
                     <button
                         type='submit'
                         className='button button-dark btn-block'
                     >
                         Sign in Now
                     </button>
-                </div>
+                </Form>
             )}
         </Formik>
     );
